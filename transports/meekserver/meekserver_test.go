cca7b4d03a98c8b2b7edc8cc453eede4d7017db3
Bluesaxorcist <joshua@operatorfoundation.org>
2019-12-12 17:37:01 -0600

    updated meek-server

--- transports/meekserver/meekserver_test.go
+++ transports/meekserver/meekserver_test.go
@@ -1,125 +1,29 @@
 package meekserver
 
 import (
-	"flag"
-	pt "git.torproject.org/pluggable-transports/goptlib.git"
-	"log"
-	"net"
-	"os"
-	"os/signal"
-	"syscall"
 	"testing"
 )
 
-//undefined: startListener and startListenerTLS
-func TestMeekServerListen(t *testing.T) {
-	meekserverTransport := meekServer{true, "cert", "key" }
-	listener := meekserverTransport.Listen("127.0.0.1:1234")
-	if listener == nil {
+//if disableTLS is true, it doesnt require the cert and key
+//func TestMeekServerListen(t *testing.T) {
+//	meekserverTransport := NewMeekTransportServer(true, "", "", "state")
+//	listener := meekserverTransport.Listen("127.0.0.1:80")
+//	if listener == nil {
+//		t.Fail()
+//	}
+//}
+
+func TestMeekServerListen2(t *testing.T) {
+	acmeEmail := "brandon@operatorfoundation.org"
+	keyFileName := "operatorrss.com"
+	meekserverTransport := NewMeekTransportServer(false, acmeEmail, keyFileName, "state")
+	if meekserverTransport == nil {
 		t.Fail()
-	}
-}
-
-//The --cert and --key options are required
-func TestMeekServerMain(t *testing.T) {
-	var disableTLS bool
-	var certFilename, keyFilename string
-	var logFilename string
-	var port int
-
-	flag.BoolVar(&disableTLS, "disable-tls", false, "don't use HTTPS")
-	flag.StringVar(&certFilename, "cert", "", "TLS certificate file (required without --disable-tls)")
-	flag.StringVar(&keyFilename, "key", "", "TLS private key file (required without --disable-tls)")
-	flag.StringVar(&logFilename, "log", "", "name of log file")
-	flag.IntVar(&port, "port", 0, "port to listen on")
-	flag.Parse()
-
-	if logFilename != "" {
-		f, err := os.OpenFile(logFilename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-		if err != nil {
-			log.Fatalf("error opening log file: %s", err)
-		}
-		defer f.Close()
-		log.SetOutput(f)
-	}
-
-	if disableTLS {
-		if certFilename != "" || keyFilename != "" {
-			log.Fatalf("The --cert and --key options are not allowed with --disable-tls.\n")
-		}
-	} else {
-		if certFilename == "" || keyFilename == "" {
-			log.Fatalf("The --cert and --key options are required.\n")
-		}
-	}
-
-	var err error
-	ptInfo, err = pt.ServerSetup([]string{ptMethodName})
-	if err != nil {
-		log.Fatalf("error in ServerSetup: %s", err)
-	}
-
-	log.Printf("starting")
-	listeners := make([]net.Listener, 0)
-	for _, bindaddr := range ptInfo.Bindaddrs {
-		if port != 0 {
-			bindaddr.Addr.Port = port
-		}
-		switch bindaddr.MethodName {
-		case ptMethodName:
-			var ln net.Listener
-			if disableTLS {
-				ln, err = startListener("tcp", bindaddr.Addr)
-			} else {
-				ln, err = startListenerTLS("tcp", bindaddr.Addr, certFilename, keyFilename)
-			}
-			if err != nil {
-				pt.SmethodError(bindaddr.MethodName, err.Error())
-				break
-			}
-			pt.Smethod(bindaddr.MethodName, ln.Addr())
-			listeners = append(listeners, ln)
-		default:
-			pt.SmethodError(bindaddr.MethodName, "no such method")
-		}
-	}
-	pt.SmethodsDone()
-
-	var numHandlers int = 0
-	var sig os.Signal
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	// Wait for first signal.
-	sig = nil
-	for sig == nil {
-		select {
-		case n := <-handlerChan:
-			numHandlers += n
-		case sig = <-sigChan:
-			log.Printf("got signal %s", sig)
-		}
-	}
-	for _, ln := range listeners {
-		ln.Close()
-	}
-
-	if sig == syscall.SIGTERM {
-		log.Printf("done")
 		return
 	}
-
-	// Wait for second signal or no more handlers.
-	sig = nil
-	for sig == nil && numHandlers != 0 {
-		select {
-		case n := <-handlerChan:
-			numHandlers += n
-		case sig = <-sigChan:
-			log.Printf("got second signal %s", sig)
-		}
+	listener := meekserverTransport.Listen("127.0.0.1:443")
+	if listener == nil {
+		t.Fail()
+		return
 	}
-
-	log.Printf("done")
 }
-
--- transports/meekserver/pt21.go
+++ transports/meekserver/pt21.go
@@ -35,88 +35,150 @@ package meekserver
 
 import (
 	"errors"
+	interconv "github.com/mufti1/interconv/package"
+	"golang.org/x/crypto/acme/autocert"
+	"log"
 	"net"
+	"net/http"
+	"strings"
 	"time"
 )
 
 // Transport that uses domain fronting to shapeshift the application network traffic
 type meekServer struct {
 	disableTLS   bool
-	certFilename string
-	keyFilename  string
+	acmeEmail    string
+	acmeHostname string
+	certManager  *autocert.Manager
 }
 
 type meekListener struct {
-	listener net.Listener
+	server *http.Server
+	state  *State
 }
 
-func (m meekListener) Accept() (net.Conn, error) {
-	conn, err := m.listener.Accept()
-	if err != nil {
-		return nil, err
+type meekServerConn struct {
+	session   *Session
+	state     *State
+	sessionID string
+}
+
+type fakeConn struct {
+	readBuffer  []byte
+	writeBuffer []byte
+}
+
+func (listener meekListener) Accept() (net.Conn, error) {
+	state := listener.state
+	state.lock.Lock()
+	defer state.lock.Unlock()
+	if state.availableSessions.Cardinality() == 0 {
+		return nil, errors.New("no connections available in accept")
+	} else {
+		sessionID := state.availableSessions.Pop()
+		sessionIDString, err := interconv.ParseString(sessionID)
+		if err != nil {
+			return nil, errors.New("could not convert sessionID to string")
+		}
+		return NewMeekServerConnection(state, sessionIDString), nil
 	}
-
-	return meekServerConn{conn}, nil
 }
 
-func (m meekListener) Close() error {
-	return m.listener.Close()
+func NewMeekServerConnection(state *State, sessionID string) net.Conn {
+	session := state.sessionMap[sessionID]
+	return meekServerConn{session, state, sessionID}
 }
 
-func (m meekListener) Addr() net.Addr {
+func (listener meekListener) Close() error {
+	return listener.server.Close()
+}
+
+func (listener meekListener) Addr() net.Addr {
 	interfaces, _ := net.Interfaces()
 	addrs, _ := interfaces[0].Addrs()
 	return addrs[0]
 }
- type meekServerConn struct {
- 	conn net.Conn
- }
 
-func (o meekServerConn) Read(b []byte) (n int, err error) {
-	return 0, errors.New("unimplemented")
+//FIXME begin critical importance
+func (conn meekServerConn) Read(b []byte) (n int, err error) {
+	if len(conn.session.Or.readBuffer) == 0 {
+		return 0, nil
+	} else {
+		copy(b, conn.session.Or.readBuffer)
+		conn.session.Or.readBuffer = conn.session.Or.readBuffer[:0]
+	}
+	return len(b), nil
 }
 
-func (o meekServerConn) Write(b []byte) (n int, err error) {
-	return 0, errors.New("unimplemented")
+func (conn meekServerConn) Write(b []byte) (n int, err error) {
+	conn.session.Or.writeBuffer = append(conn.session.Or.writeBuffer, b...)
+	return len(b), nil
 }
 
-func (o meekServerConn) Close() error {
-	return errors.New("unimplemented")
-}
-
-func (o meekServerConn) LocalAddr() net.Addr {
+func (conn meekServerConn) Close() error {
+	conn.state.CloseSession(conn.sessionID)
 	return nil
 }
 
-func (o meekServerConn) RemoteAddr() net.Addr {
+//end critical importance
+func (conn meekServerConn) LocalAddr() net.Addr {
 	return nil
 }
 
-func (o meekServerConn) SetDeadline(t time.Time) error {
+func (conn meekServerConn) RemoteAddr() net.Addr {
+	return nil
+}
+
+func (conn meekServerConn) SetDeadline(t time.Time) error {
 	return errors.New("unimplemented")
 }
 
-func (o meekServerConn) SetReadDeadline(t time.Time) error {
+func (conn meekServerConn) SetReadDeadline(t time.Time) error {
 	return errors.New("unimplemented")
 }
 
-func (o meekServerConn) SetWriteDeadline(t time.Time) error {
+func (conn meekServerConn) SetWriteDeadline(t time.Time) error {
 	return errors.New("unimplemented")
 }
 
 // Public initializer method to get a new meek transport
 
-func NewMeekServer(disableTLS bool, certFilename string, keyFilename string) *meekServer {
+func NewMeekTransportServer(disableTLS bool, acmeEmail string, acmeHostnamesCommas string, stateDir string) *meekServer {
+	var certManager *autocert.Manager
 	if disableTLS {
-		if certFilename != "" || keyFilename != "" {
+		if acmeEmail != "" || acmeHostnamesCommas != "" {
 			return nil
 		}
 	} else {
-		if certFilename == "" || keyFilename == "" {
+		if acmeEmail == "" || acmeHostnamesCommas == "" {
 			return nil
 		}
+		if acmeHostnamesCommas != "" {
+			acmeHostnames := strings.Split(acmeHostnamesCommas, ",")
+			log.Printf("ACME hostnames: %q", acmeHostnames)
+
+			// The ACME HTTP-01 responder only works when it is running on
+			// port 80.
+			// https://github.com/ietf-wg-acme/acme/blob/master/draft-ietf-acme-acme.md#http-challenge
+
+			var cache autocert.Cache
+			cacheDir, err := getCertificateCacheDir(stateDir)
+			if err == nil {
+				log.Printf("caching ACME certificates in directory %q", cacheDir)
+				cache = autocert.DirCache(cacheDir)
+			} else {
+				log.Printf("disabling ACME certificate cache: %s", err)
+			}
+
+			certManager = &autocert.Manager{
+				Prompt:     autocert.AcceptTOS,
+				HostPolicy: autocert.HostWhitelist(acmeHostnames...),
+				Email:      acmeEmail,
+				Cache:      cache,
+			}
+		}
 	}
-	return &meekServer{disableTLS, certFilename, keyFilename}
+	return &meekServer{disableTLS, acmeEmail, acmeHostnamesCommas, certManager}
 }
 
 // Methods that implement the base.Transport interface
@@ -124,206 +186,36 @@ func NewMeekServer(disableTLS bool, certFilename string, keyFilename string) *me
 // The meek transport does not have a corresponding server, only a client
 func (transport *meekServer) Listen(address string) net.Listener {
 	var ln net.Listener
+	var state *State
 	var err error
 	addr, resolverr := net.ResolveTCPAddr("tcp", address)
 	if resolverr != nil {
 		return ln
 	}
+	acmeAddr := net.TCPAddr{
+		IP:   addr.IP,
+		Port: 80,
+		Zone: "",
+	}
+	acmeAddr.Port = 80
+	log.Printf("starting HTTP-01 ACME listener on %s", acmeAddr.String())
+	lnHTTP01, err := net.ListenTCP("tcp", &acmeAddr)
+	if err != nil {
+		log.Printf("error opening HTTP-01 ACME listener: %s", err)
+		return nil
+	}
+	go func() {
+		log.Fatal(http.Serve(lnHTTP01, transport.certManager.HTTPHandler(nil)))
+	}()
+	var server *http.Server
 	if transport.disableTLS {
-		ln, err = startListener("tcp", addr)
+		server, state, err = startServer(addr)
 	} else {
-		ln, err = startListenerTLS("tcp", addr, transport.certFilename, transport.keyFilename)
+		server, state, err = startServerTLS(addr, transport.certManager.GetCertificate)
 	}
 	if err != nil {
-		return ln
+
+		return nil
 	}
-
-	return meekListener{ln}
+	return meekListener{server, state}
 }
-
-// End methods that implement the base.Transport interface
-
-//func (ca *meekClientArgs) String() string {
-//	return "meek" + ":" + ca.front + ":" + ca.url.String()
-//}
-//
-//func newClientArgs(url string) (ca *meekClientArgs, err error) {
-//	ca = &meekClientArgs{}
-//
-//	// Parse the URL argument.
-//	ca.url, err = gourl.Parse(url)
-//	if err != nil {
-//		return nil, fmt.Errorf("malformed url: '%s'", url)
-//	}
-//	switch ca.url.Scheme {
-//	case "http", "https":
-//	default:
-//		return nil, fmt.Errorf("invalid scheme: '%s'", ca.url.Scheme)
-//	}
-//
-//	return ca, nil
-//}
-//
-//func newClientArgsWithFront(url string, front string) (ca *meekClientArgs, err error) {
-//	ca = &meekClientArgs{}
-//
-//	// Parse the URL argument.
-//	ca.url, err = gourl.Parse(url)
-//	if err != nil {
-//		return nil, fmt.Errorf("malformed url: '%s'", url)
-//	}
-//	switch ca.url.Scheme {
-//	case "http", "https":
-//	default:
-//		return nil, fmt.Errorf("invalid scheme: '%s'", ca.url.Scheme)
-//	}
-//
-//	// Parse the (optional) front argument.
-//	ca.front = front
-//
-//	return ca, nil
-//}
-//
-//// Implementation of base.TransportConn, which also requires implementing net.Conn
-//type meekConn struct {
-//	sync.Mutex
-//
-//	args      *meekClientArgs
-//	sessionID string
-//	transport *http.Transport
-//
-//	workerRunning   bool
-//	workerWrChan    chan []byte
-//	workerRdChan    chan []byte
-//	workerCloseChan chan bool
-//	rdBuf           *bytes.Buffer
-//}
-//
-//// Private initializer methods
-//
-//func newMeekClientConn(addr string, ca *meekClientArgs) (*meekConn, error) {
-//	id, err := newSessionID()
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	tr := &http.Transport{}
-//	conn := &meekConn{
-//		args:            ca,
-//		sessionID:       id,
-//		transport:       tr,
-//		workerRunning:   true,
-//		workerWrChan:    make(chan []byte, maxChanBacklog),
-//		workerRdChan:    make(chan []byte, maxChanBacklog),
-//		workerCloseChan: make(chan bool),
-//	}
-//
-//	// Start the I/O worker.
-//	go conn.ioWorker()
-//
-//	return conn, nil
-//}
-//
-//// End initializer methods
-//// End methods that implement the base.TransportConn interface
-//
-//// Methods implementing net.Conn
-//func (c *meekConn) Read(p []byte) (n int, err error) {
-//	// If there is data left over from the previous read,
-//	// service the request using the buffered data.
-//	if c.rdBuf != nil {
-//		if c.rdBuf.Len() == 0 {
-//			panic("empty read buffer")
-//		}
-//		n, err = c.rdBuf.Read(p)
-//		if c.rdBuf.Len() == 0 {
-//			c.rdBuf = nil
-//		}
-//		return
-//	}
-//
-//	// Wait for the worker to enqueue more incoming data.
-//	b, ok := <-c.workerRdChan
-//	if !ok {
-//		// Close() was called and the worker's shutting down.
-//		return 0, io.ErrClosedPipe
-//	}
-//
-//	// Ew, an extra copy, but who am I kidding, it's meek.
-//	buf := bytes.NewBuffer(b)
-//	n, err = buf.Read(p)
-//	if buf.Len() > 0 {
-//		// If there's data pending, stash the buffer so the next
-//		// Read() call will use it to fulfuill the Read().
-//		c.rdBuf = buf
-//	}
-//	return
-//}
-//
-//func (c *meekConn) Write(b []byte) (n int, err error) {
-//	// Check to see if the connection is actually open.
-//	c.Lock()
-//	closed := !c.workerRunning
-//	c.Unlock()
-//	if closed {
-//		return 0, io.ErrClosedPipe
-//	}
-//
-//	if len(b) == 0 {
-//		return 0, nil
-//	}
-//
-//	// Copy the data to be written to a new slice, since
-//	// we return immediately after queuing and the peer can
-//	// happily reuse `b` before data has been sent.
-//	toWrite := len(b)
-//	b2 := make([]byte, toWrite)
-//	copy(b2, b)
-//	if ok := c.enqueueWrite(b2); !ok {
-//		// Technically we did enqueue data, but the worker's
-//		// got closed out from under us.
-//		return 0, io.ErrClosedPipe
-//	}
-//	runtime.Gosched()
-//	return len(b), nil
-//}
-//
-//func (c *meekConn) Close() error {
-//	// Ensure that we do this once and only once.
-//	c.Lock()
-//	defer c.Unlock()
-//	if !c.workerRunning {
-//		return nil
-//	}
-//
-//	// Tear down the worker.
-//	c.workerRunning = false
-//	c.workerCloseChan <- true
-//
-//	return nil
-//}
-//
-//func (c *meekConn) LocalAddr() net.Addr {
-//	return &net.IPAddr{IP: loopbackAddr}
-//}
-//
-//func (c *meekConn) RemoteAddr() net.Addr {
-//	return c.args
-//}
-//
-//func (c *meekConn) SetDeadline(t time.Time) error {
-//	return ErrNotSupported
-//}
-//
-//func (c *meekConn) SetReadDeadline(t time.Time) error {
-//	return ErrNotSupported
-//}
-//
-//func (c *meekConn) SetWriteDeadline(t time.Time) error {
-//	return ErrNotSupported
-//}
-//
-//// End of methods implementing net.Conn
-//// Force type checks to make sure that instances conform to interfaces
-//var _ net.Conn = (*meekConn)(nil)
-//var _ net.Addr = (*meekClientArgs)(nil)
