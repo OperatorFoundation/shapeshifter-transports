17de9f3215a3be945b421fbd9d4bcc1049987f12
Consuelita <mafalda@mafaldas-mbp.attlocal.net>
2020-01-08 17:48:48 -0600

    Update Optimizer.go

--- transports/Replicant/Optimizer.go
+++ transports/Replicant/Optimizer.go
@@ -1,7 +1,6 @@
 package replicant
 
 import (
-	"errors"
 	"golang.org/x/net/proxy"
 	"net"
 )
@@ -13,13 +12,16 @@ type Transport struct {
 	Dialer  proxy.Dialer
 }
 
+// TODO: the dial we call currently does not return an error
 func (transport Transport) Dial() (net.Conn, error) {
 	replicantTransport:= New(transport.Config, transport.Dialer)
-	conn, err:= replicantTransport.Dial(transport.Address), errors.New("connection failed")
-	if err != nil {
-		return nil, err
-	} else {
-		return conn, nil
-	}
+	conn := replicantTransport.Dial(transport.Address)
+	//conn, err:= replicantTransport.Dial(transport.Address), errors.New("connection failed")
+	//if err != nil {
+	//	return nil, err
+	//} else {
+	//	return conn, nil
+	//}
+	return conn, nil
 }
 
