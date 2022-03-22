baa180e97b1369f0ba07bf7bf484f5111f111c36
CryptoSax <joshua@operatorfoundation.org>
2022-03-22 16:10:57 -0500

    added v2 folder

--- tagpushall.sh
+++ tagpushall.sh
@@ -1,7 +1,7 @@
 #git tag $1
 #git push origin $1
 
-for transport in obfs4 obfs2 shadow Replicant Optimizer Dust meeklite meekserver
+for transport in obfs4 obfs2 shadow Replicant Optimizer Dust meeklite meekserver StarBridge
 do
   ./tagpush.sh $transport $1
 done
